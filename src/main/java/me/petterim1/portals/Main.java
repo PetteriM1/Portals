e845f392290bdb4e262e6c0e923dd3d6f065c91b
PetteriM1 <petteri.malkavaara1@gmail.com>
2019-04-18 21:11:14 +0300

    Add option to fix portal loop with server changes
    Fix #1

--- src/main/java/me/petterim1/portals/Main.java
+++ src/main/java/me/petterim1/portals/Main.java
@@ -8,12 +8,21 @@ import cn.nukkit.utils.ConfigSection;
 
 public class Main extends PluginBase {
 
+    private static final int configVersion = 2;
+
     public static Config config;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         config = getConfig();
+        if (config.getInt("configVersion") != configVersion) {
+            getLogger().warning("Outdated config file, trying to update it automatically");
+            config.set("configVersion", configVersion);
+            config.set("resetPosition", false);
+            config.save();
+            config = getConfig();
+        }
         getServer().getScheduler().scheduleDelayedRepeatingTask(this, new Task(), 2, 2);
     }
 }
@@ -30,12 +39,18 @@ class Task extends Thread {
                         if (c.getString("rotation").equals("x")) {
                             if (Math.round(p.x * 2) / 2.0 == c.getInt("x") && Math.round(p.z) <= c.getInt("z") + c.getInt("width") && Math.round(p.z) >= c.getInt("z")) {
                                 if (p.y >= c.getDouble("y") && p.y <= c.getDouble("y") + c.getDouble("height")) {
+                                    if (Main.config.getBoolean("resetPosition")) {
+                                        p.teleport(p.getLevel().getSafeSpawn());
+                                    }
                                     Server.getInstance().dispatchCommand(p, c.getString("command"));
                                 }
                             }
                         } else if (c.getString("rotation").equals("z")) {
                             if (Math.round(p.z * 2) / 2.0 == c.getInt("z") && Math.round(p.x) <= c.getInt("x") + c.getInt("width") && Math.round(p.x) >= c.getInt("x")) {
                                 if (p.y >= c.getDouble("y") && p.y <= c.getDouble("y") + c.getDouble("height")) {
+                                    if (Main.config.getBoolean("resetPosition")) {
+                                        p.teleport(p.getLevel().getSafeSpawn());
+                                    }
                                     Server.getInstance().dispatchCommand(p, c.getString("command"));
                                 }
                             }
